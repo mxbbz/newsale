07eb4086d27dda5f145e518ab767095fe85b6b01
颜秉南 <2337460717@qq.com>
2022-05-17 15:50:20 +0800

    add logo

---
+++ src/views/home/banner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="banner">
+    <div class="wrapper">
+      <div class="banner-list">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ .wrapper{
+    width: 1240px;
+    margin: 0 auto;
+  }
+</style>
